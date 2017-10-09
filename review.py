08b55f21d9eafd3bb15e2cb279acd256ebffd8c4
Zh.EastSun <947519418@qq.com>
2017-10-09 19:26:58 +0800

    复习

---
+++ review.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#一般字符串输出
+name = "zdy-windows"
+
+print name
+
+#格式化输出
+year = 1997
+month = 1
+day = 17
+print "%d-%d-%d"%(year,month,day)
+
+#整除
+print 5//2
+
+#乘方
+print 5**2
