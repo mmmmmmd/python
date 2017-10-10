eaae33ddd7920763decda81cc5c24d9962cf39cc
Zh.EastSun <947519418@qq.com>
2017-10-10 12:44:56 +0800

    循环语法练习

---
+++ 循环练习.py
@@ -0,0 +1,14 @@
+# -*- coding:utf-8 -*-
+#while循环实现1-100的累加
+sum = 0
+i=1
+while i<=100:
+    sum = sum+i
+    i = i+1
+print sum
+
+sum=0
+#for循环实现1-100的累加
+for j in range(1,101):
+    sum = sum+j
+print sum
